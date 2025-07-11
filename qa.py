4d55d2cc593ac9d225cedd731b5d4c303000b7e0
ElijahMuessemeyer <david.muessemeyer@csuglobal.edu>
2025-07-10 21:15:41 -0500

    Delete qa.py

--- qa.py
+++
@@ -1,7 +0,0 @@
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import BaseRetriever
-
-def build_qa_chain(retriever: BaseRetriever) -> RetrievalQA:
-    llm = ChatOpenAI(temperature=0)
-    return RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
